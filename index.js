bf3822275bbce7fd57aa886d844a7d4e8b42554f
Lana Kisel <svetlana.kisel.ca@gmail.com>
2023-09-01 14:07:21 -0700

    loads domcontent event listener

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', dom);
+
+function dom(e) {
+   let text = document.getElementById('text');
+   text.textContent = "This is really cool!"
+}
+
+
+//console.log( "This conosole.log() fires when index.js loads - before DOMContentLoaded is triggered")
